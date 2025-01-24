c8548aa18de11ac410f16727dc5b88f8ec8c4cda
dawe <dawedawe@posteo.de>
2025-01-25 00:13:49 +0100

    move to elm-like architecture

--- src/app.rs
+++ src/app.rs
@@ -5,26 +5,26 @@ use ratatui::{
     style::{Color, Modifier, Style},
     text::{Line, Span, Text},
     widgets::{Bar, BarChart, BarGroup, Block, Borders, Paragraph},
-    DefaultTerminal, Frame,
+    Frame,
 };
 use rusistor::{self, Resistor};
 use tui_input::backend::crossterm::EventHandler;
 use tui_input::Input;
 
 #[derive(Debug)]
-pub struct App {
-    running: bool,
-    resistance_input: Input,
-    tolerance_input: Input,
-    tcr_input: Input,
-    focus: usize,
-    resistor: Option<Resistor>,
-    error: Option<String>,
+pub struct Model {
+    pub running: bool,
+    pub resistance_input: Input,
+    pub tolerance_input: Input,
+    pub tcr_input: Input,
+    pub focus: usize,
+    pub resistor: Option<Resistor>,
+    pub error: Option<String>,
 }
 
-impl Default for App {
-    fn default() -> App {
-        App {
+impl Default for Model {
+    fn default() -> Model {
+        Model {
             running: true,
             resistance_input: Input::default(),
             tolerance_input: Input::default(),
@@ -36,243 +36,242 @@ impl Default for App {
     }
 }
 
-impl App {
-    pub fn new() -> Self {
-        Self::default()
-    }
+pub fn view(model: &Model, frame: &mut Frame) {
+    let chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .margin(2)
+        .constraints(
+            [
+                Constraint::Length(1),
+                Constraint::Length(3),
+                Constraint::Min(1),
+            ]
+            .as_ref(),
+        )
+        .split(frame.area());
+    let input_rects = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([
+            Constraint::Ratio(1, 3),
+            Constraint::Ratio(1, 3),
+            Constraint::Ratio(1, 3),
+        ])
+        .split(chunks[1]);
 
-    pub fn run(mut self, mut terminal: DefaultTerminal) -> Result<()> {
-        while self.running {
-            terminal.draw(|frame| self.draw(frame))?;
-            self.handle_crossterm_events()?;
-        }
-        Ok(())
-    }
+    let main_rects = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([
+            Constraint::Percentage(10),
+            Constraint::Percentage(80),
+            Constraint::Percentage(10),
+        ])
+        .split(chunks[2]);
 
-    fn draw(&mut self, frame: &mut Frame) {
-        let chunks = Layout::default()
-            .direction(Direction::Vertical)
-            .margin(2)
-            .constraints(
-                [
-                    Constraint::Length(1),
-                    Constraint::Length(3),
-                    Constraint::Min(1),
-                ]
-                .as_ref(),
-            )
-            .split(frame.area());
-        let input_rects = Layout::default()
-            .direction(Direction::Horizontal)
-            .constraints([
-                Constraint::Ratio(1, 3),
-                Constraint::Ratio(1, 3),
-                Constraint::Ratio(1, 3),
-            ])
-            .split(chunks[1]);
+    let help_msg_rect = chunks[0];
+    let resistance_rect = input_rects[0];
+    let tolerance_rect = input_rects[1];
+    let tcr_rect = input_rects[2];
+    let main_rect = main_rects[1];
 
-        let main_rects = Layout::default()
-            .direction(Direction::Horizontal)
-            .constraints([
-                Constraint::Percentage(10),
-                Constraint::Percentage(80),
-                Constraint::Percentage(10),
-            ])
-            .split(chunks[2]);
+    let (msg, style) = (
+        vec![
+            Span::raw("Press "),
+            Span::styled("Esc", Style::default().add_modifier(Modifier::BOLD)),
+            Span::raw(" to exit, "),
+            Span::styled("Tab", Style::default().add_modifier(Modifier::BOLD)),
+            Span::raw(" to move to the next input, "),
+            Span::styled("Enter", Style::default().add_modifier(Modifier::BOLD)),
+            Span::raw(" to determine the resistor."),
+        ],
+        Style::default(),
+    );
+    let text = Text::from(Line::from(msg)).style(style);
+    let help_message = Paragraph::new(text);
+    frame.render_widget(help_message, help_msg_rect);
 
-        let help_msg_rect = chunks[0];
-        let resistance_rect = input_rects[0];
-        let tolerance_rect = input_rects[1];
-        let tcr_rect = input_rects[2];
-        let main_rect = main_rects[1];
-
-        let (msg, style) = (
-            vec![
-                Span::raw("Press "),
-                Span::styled("Esc", Style::default().add_modifier(Modifier::BOLD)),
-                Span::raw(" to exit, "),
-                Span::styled("Tab", Style::default().add_modifier(Modifier::BOLD)),
-                Span::raw(" to move to the next input, "),
-                Span::styled("Enter", Style::default().add_modifier(Modifier::BOLD)),
-                Span::raw(" to determine the resistor."),
-            ],
-            Style::default(),
+    let resistance_width = resistance_rect.width.max(3) - 3; // keep 2 for borders and 1 for cursor
+    let resistance_scroll = model
+        .resistance_input
+        .visual_scroll(resistance_width as usize);
+    let resistance_paragraph = Paragraph::new(model.resistance_input.value())
+        .style(Style::default().fg(Color::Yellow))
+        .scroll((0, resistance_scroll as u16))
+        .block(
+            Block::default()
+                .borders(Borders::ALL)
+                .title(" Resistance (Ohm) "),
         );
-        let text = Text::from(Line::from(msg)).style(style);
-        let help_message = Paragraph::new(text);
-        frame.render_widget(help_message, help_msg_rect);
+    frame.render_widget(resistance_paragraph, resistance_rect);
 
-        let resistance_width = resistance_rect.width.max(3) - 3; // keep 2 for borders and 1 for cursor
-        let resistance_scroll = self
-            .resistance_input
-            .visual_scroll(resistance_width as usize);
-        let resistance_paragraph = Paragraph::new(self.resistance_input.value())
-            .style(Style::default().fg(Color::Yellow))
-            .scroll((0, resistance_scroll as u16))
-            .block(
-                Block::default()
-                    .borders(Borders::ALL)
-                    .title(" Resistance (Ohm) "),
-            );
-        frame.render_widget(resistance_paragraph, resistance_rect);
+    let tolerance_width = tolerance_rect.width.max(3) - 3; // keep 2 for borders and 1 for cursor
+    let tolerance_scroll = model
+        .tolerance_input
+        .visual_scroll(tolerance_width as usize);
+    let tolerance_paragraph = Paragraph::new(model.tolerance_input.value())
+        .style(Style::default().fg(Color::Yellow))
+        .scroll((0, tolerance_scroll as u16))
+        .block(Block::default().borders(Borders::ALL).title(" Tolerance "));
+    frame.render_widget(tolerance_paragraph, tolerance_rect);
 
-        let tolerance_width = tolerance_rect.width.max(3) - 3; // keep 2 for borders and 1 for cursor
-        let tolerance_scroll = self.tolerance_input.visual_scroll(tolerance_width as usize);
-        let tolerance_paragraph = Paragraph::new(self.tolerance_input.value())
-            .style(Style::default().fg(Color::Yellow))
-            .scroll((0, tolerance_scroll as u16))
-            .block(Block::default().borders(Borders::ALL).title(" Tolerance "));
-        frame.render_widget(tolerance_paragraph, tolerance_rect);
+    let tcr_width = tcr_rect.width.max(3) - 3; // keep 2 for borders and 1 for cursor
+    let tcr_scroll = model.tcr_input.visual_scroll(tcr_width as usize);
+    let tcr_paragraph = Paragraph::new(model.tcr_input.value())
+        .style(Style::default().fg(Color::Yellow))
+        .scroll((0, tcr_scroll as u16))
+        .block(Block::default().borders(Borders::ALL).title(" TCR "));
+    frame.render_widget(tcr_paragraph, tcr_rect);
 
-        let tcr_width = tcr_rect.width.max(3) - 3; // keep 2 for borders and 1 for cursor
-        let tcr_scroll = self.tcr_input.visual_scroll(tcr_width as usize);
-        let tcr_paragraph = Paragraph::new(self.tcr_input.value())
-            .style(Style::default().fg(Color::Yellow))
-            .scroll((0, tcr_scroll as u16))
-            .block(Block::default().borders(Borders::ALL).title(" TCR "));
-        frame.render_widget(tcr_paragraph, tcr_rect);
+    // Make the cursor visible and ask tui-rs to put it at the specified coordinates after rendering
+    let (rect, input, scroll) = match model.focus {
+        0 => (resistance_rect, &model.resistance_input, resistance_scroll),
+        1 => (tolerance_rect, &model.tolerance_input, tolerance_scroll),
+        _ => (tcr_rect, &model.tcr_input, tcr_scroll),
+    };
+    frame.set_cursor_position((
+        // Put cursor past the end of the input text
+        rect.x + ((input.visual_cursor()).max(scroll) - scroll) as u16 + 1,
+        // Move one line down, from the border to the input line
+        rect.y + 1,
+    ));
 
-        // Make the cursor visible and ask tui-rs to put it at the specified coordinates after rendering
-        let (rect, input, scroll) = match self.focus {
-            0 => (resistance_rect, &self.resistance_input, resistance_scroll),
-            1 => (tolerance_rect, &self.tolerance_input, tolerance_scroll),
-            _ => (tcr_rect, &self.tcr_input, tcr_scroll),
-        };
-        frame.set_cursor_position((
-            // Put cursor past the end of the input text
-            rect.x + ((input.visual_cursor()).max(scroll) - scroll) as u16 + 1,
-            // Move one line down, from the border to the input line
-            rect.y + 1,
-        ));
-
-        match &self.resistor {
-            Some(resistor) => {
-                let colors = resistor
-                    .bands()
-                    .iter()
-                    .map(|c| rusistor_color_to_ratatui_color(c))
-                    .collect::<Vec<(Color, String)>>();
-                let specs = resistor.specs();
-                let chart = barchart(&colors, specs.ohm, specs.tolerance, specs.tcr);
-                frame.render_widget(chart, main_rect);
+    match &model.resistor {
+        Some(resistor) => {
+            let colors = resistor
+                .bands()
+                .iter()
+                .map(|c| rusistor_color_to_ratatui_color(c))
+                .collect::<Vec<(Color, String)>>();
+            let specs = resistor.specs();
+            let chart = barchart(&colors, specs.ohm, specs.tolerance, specs.tcr);
+            frame.render_widget(chart, main_rect);
+        }
+        None => {
+            if let Some(e) = &model.error {
+                let text = Text::from(e.to_string());
+                let error_message = Paragraph::new(text);
+                frame.render_widget(error_message, main_rect);
             }
-            None => {
-                if let Some(e) = &self.error {
-                    let text = Text::from(e.to_string());
-                    let error_message = Paragraph::new(text);
-                    frame.render_widget(error_message, main_rect);
+        }
+    }
+}
+
+pub enum Msg {
+    Determine,
+    Focus { idx: usize },
+    Exit,
+}
+
+pub fn handle_event(model: &mut Model) -> Result<Option<Msg>> {
+    handle_crossterm_events(model)
+}
+
+fn handle_crossterm_events(model: &mut Model) -> Result<Option<Msg>> {
+    match event::read()? {
+        // it's important to check KeyEventKind::Press to avoid handling key release events
+        Event::Key(key) if key.kind == KeyEventKind::Press => Result::Ok(on_key_event(model, key)),
+        Event::Mouse(_) => Result::Ok(None),
+        Event::Resize(_, _) => Result::Ok(None),
+        _ => Result::Ok(None),
+    }
+}
+
+fn on_key_event(model: &mut Model, key: KeyEvent) -> Option<Msg> {
+    match key.code {
+        KeyCode::Enter => Some(Msg::Determine),
+        KeyCode::Tab => Some(Msg::Focus {
+            idx: (model.focus + 1) % 3,
+        }),
+        KeyCode::Esc => Some(Msg::Exit),
+        _ => {
+            let target_input = match model.focus {
+                0 => &mut model.resistance_input,
+                1 => &mut model.tolerance_input,
+                _ => &mut model.tcr_input,
+            };
+            target_input.handle_event(&Event::Key(key));
+            None
+        }
+    }
+}
+
+pub fn update(model: &mut Model, msg: Msg) {
+    match msg {
+        Msg::Determine => {
+            model.resistor = None;
+            let resistance_input_value = model.resistance_input.value();
+
+            let resistance = match resistance_input_value.parse::<f64>() {
+                Ok(t) => Ok(t),
+                Err(e) => Err(format!("invalid input for resistance: {}", e)),
+            };
+
+            let tolerance_input_value = model.tolerance_input.value();
+            let tolerance = if tolerance_input_value.is_empty() {
+                Ok(None)
+            } else {
+                match tolerance_input_value.parse::<f64>() {
+                    Ok(t) => Ok(Some(t)),
+                    Err(e) => Err(format!("invalid input for tolerance: {}", e)),
                 }
-            }
-        }
-    }
+            };
 
-    /// Reads the crossterm events and updates the state of [`App`].
-    ///
-    /// If your application needs to perform work in between handling events, you can use the
-    /// [`event::poll`] function to check if there are any events available with a timeout.
-    fn handle_crossterm_events(&mut self) -> Result<()> {
-        match event::read()? {
-            // it's important to check KeyEventKind::Press to avoid handling key release events
-            Event::Key(key) if key.kind == KeyEventKind::Press => self.on_key_event(key),
-            Event::Mouse(_) => {}
-            Event::Resize(_, _) => {}
-            _ => {}
-        }
-        Ok(())
-    }
+            let tcr_input_value = model.tcr_input.value();
+            let tcr = if tcr_input_value.is_empty() {
+                Ok(None)
+            } else {
+                match tcr_input_value.parse::<u32>() {
+                    Ok(t) => Ok(Some(t)),
+                    Err(e) => Err(format!("invalid input for tcr: {}", e)),
+                }
+            };
 
-    /// Handles the key events and updates the state of [`App`].
-    fn on_key_event(&mut self, key: KeyEvent) {
-        match key.code {
-            KeyCode::Enter => {
-                let resistance_input_value = self.resistance_input.value();
-                self.resistor = None;
-
-                let resistance = match resistance_input_value.parse::<f64>() {
-                    Ok(t) => Ok(t),
-                    Err(e) => Err(format!("invalid input for resistance: {}", e)),
-                };
-
-                let tolerance_input_value = self.tolerance_input.value();
-                let tolerance = if tolerance_input_value.is_empty() {
-                    Ok(None)
-                } else {
-                    match tolerance_input_value.parse::<f64>() {
-                        Ok(t) => Ok(Some(t)),
-                        Err(e) => Err(format!("invalid input for tolerance: {}", e)),
-                    }
-                };
-
-                let tcr_input_value = self.tcr_input.value();
-                let tcr = if tcr_input_value.is_empty() {
-                    Ok(None)
-                } else {
-                    match tcr_input_value.parse::<u32>() {
-                        Ok(t) => Ok(Some(t)),
-                        Err(e) => Err(format!("invalid input for tcr: {}", e)),
-                    }
-                };
-
-                match (resistance, tolerance, tcr) {
-                    (Ok(resistance), Ok(tolerance), Ok(tcr)) => {
-                        match Resistor::determine(resistance, tolerance, tcr) {
-                            Ok(resitor) => {
-                                self.error = None;
-                                self.resistor = Some(resitor)
-                            }
-                            Err(e) => {
-                                self.error = Some(format!(
-                                    "could not determine a resistor for these inputs: {}",
-                                    e
-                                ));
-                            }
+            match (resistance, tolerance, tcr) {
+                (Ok(resistance), Ok(tolerance), Ok(tcr)) => {
+                    match Resistor::determine(resistance, tolerance, tcr) {
+                        Ok(resitor) => {
+                            model.error = None;
+                            model.resistor = Some(resitor)
                         }
-                    }
-                    (res, tol, tcr) => {
-                        let mut error_msg: String = String::from("");
-                        if let Err(res_error) = res {
-                            error_msg.push_str(res_error.to_string().as_str());
-                        }
-                        if let Err(tol_error) = tol {
-                            error_msg.push('\n');
-                            error_msg.push_str(tol_error.to_string().as_str());
-                        }
-                        if let Err(tcr_error) = tcr {
-                            error_msg.push('\n');
-                            error_msg.push_str(tcr_error.to_string().as_str());
-                        }
-                        if error_msg.is_empty() {
-                            self.error = None
-                        } else {
-                            self.error = Some(error_msg)
+                        Err(e) => {
+                            model.error = Some(format!(
+                                "could not determine a resistor for these inputs: {}",
+                                e
+                            ));
                         }
                     }
                 }
+                (res, tol, tcr) => {
+                    let mut error_msg: String = String::from("");
+                    if let Err(res_error) = res {
+                        error_msg.push_str(res_error.to_string().as_str());
+                    }
+                    if let Err(tol_error) = tol {
+                        error_msg.push('\n');
+                        error_msg.push_str(tol_error.to_string().as_str());
+                    }
+                    if let Err(tcr_error) = tcr {
+                        error_msg.push('\n');
+                        error_msg.push_str(tcr_error.to_string().as_str());
+                    }
+                    if error_msg.is_empty() {
+                        model.error = None
+                    } else {
+                        model.error = Some(error_msg)
+                    }
+                }
+            }
 
-                self.reset_inputs();
-            }
-            KeyCode::Tab => self.focus = (self.focus + 1) % 3,
-            KeyCode::Esc => self.quit(),
-            _ => {
-                let target_input = match self.focus {
-                    0 => &mut self.resistance_input,
-                    1 => &mut self.tolerance_input,
-                    _ => &mut self.tcr_input,
-                };
-                target_input.handle_event(&Event::Key(key));
-            }
+            model.resistance_input.reset();
+            model.tolerance_input.reset();
+            model.tcr_input.reset();
+            model.focus = 0;
         }
-    }
-
-    fn quit(&mut self) {
-        self.running = false;
-    }
-
-    fn reset_inputs(&mut self) {
-        self.resistance_input.reset();
-        self.tolerance_input.reset();
-        self.tcr_input.reset();
-        self.focus = 0;
+        Msg::Focus { idx: i } => {
+            model.focus = i;
+        }
+        Msg::Exit => {
+            model.running = false;
+        }
     }
 }
 

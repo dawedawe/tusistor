04fed66d704c6a6ee69ae516bf60c4222b18c808
dawe <dawedawe@posteo.de>
2025-02-16 18:33:34 +0100

    cleanup mainrs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-pub use app::{handle_event, update, view, Model};
-use color_eyre::eyre::Ok;
-
 pub mod app;
+
+use app::{handle_event, update, view, Model};
+use color_eyre::eyre::Ok;
 
 fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
